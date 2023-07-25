33de0392fc13237648bd76ea4304da2911cfa1f6
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-25 15:14:20 +0200

    added routes to manage correctly create and store functions of AdminComicController

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,6 @@ Route::get('/', function () {
 });
 
 Route::get('admin/comics', [AdminComicController::class, 'index'])->name('admin.comics.index');
+Route::get('admin/comics/create', [AdminComicController::class, 'create'])->name('admin.comics.create');
+Route::post('admin/comics', [AdminComicController::class, 'store'])->name('admin.comics.store');
 Route::get('admin/comics/{id}', [AdminComicController::class, 'show'])->name('admin.comics.show');
