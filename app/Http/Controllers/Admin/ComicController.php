1549887b3173351e81505f1b6fa338190b0d8eb5
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-25 13:57:57 +0200

    fixed pagination of admin index

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -15,7 +15,7 @@ class ComicController extends Controller
      */
     public function index()
     {
-        $comicList = Comic::paginate(10);
+        $comicList = Comic::paginate(6);
         return view('admin.comics.index', compact('comicList'));
     }
 
